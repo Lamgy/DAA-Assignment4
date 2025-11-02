698de26f6086ce64ae29bb1852ef6c05a092e111
Lamgy <kloverpoopa@gmail.com>
2025-11-03 02:08:56 +0500

    feat/algorithms - added long path

--- src/main/java/graph/dagsp/DAGShortestPath.java
+++ src/main/java/graph/dagsp/DAGShortestPath.java
@@ -39,4 +39,40 @@ public class DAGShortestPath {
         }
         return dist;
     }
+
+    public Map<String, Double> longestPath(Map<String, List<Edge>> adj, String src) {
+        Map<String, List<String>> simple = new HashMap<>();
+        for (var e : adj.entrySet()) {
+            List<String> vs = new ArrayList<>();
+            for (Edge ed : e.getValue()) vs.add(ed.getTo());
+            simple.put(e.getKey(), vs);
+        }
+
+        List<String> topoOrder = new KahnTopo(metrics).sort(simple);
+
+        Map<String, Double> dist = new HashMap<>();
+        for (String u : adj.keySet()) dist.put(u, Double.NEGATIVE_INFINITY);
+        dist.put(src, 0.0);
+
+        for (String u : topoOrder) {
+            if (dist.get(u) == Double.NEGATIVE_INFINITY) continue;
+            for (Edge e : adj.getOrDefault(u, List.of())) {
+                metrics.relaxations++;
+                double newDist = dist.get(u) + e.getWeight();
+                if (newDist > dist.get(e.getTo())) {
+                    dist.put(e.getTo(), newDist);
+                }
+            }
+        }
+
+        return dist;
+    }
+
+    public String getCriticalEnd(Map<String, Double> longestDist) {
+        return longestDist.entrySet()
+                .stream()
+                .max(Map.Entry.comparingByValue())
+                .map(Map.Entry::getKey)
+                .orElse(null);
+    }
 }
